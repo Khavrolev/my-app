45814291a711858ace9c5c89b8c6cfbca650c18b
Sergey Khavrolev <sergey.khavrolev@coolbet.com>
2022-12-03 11:33:43 +0300

    init test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+
+const STEP = 25;
+
+const keys = {
+  left: 37,
+  up: 38,
+  right: 39,
+  down: 40,
+};
 
 function App() {
+  const [top, setTop] = useState(0);
+  const [left, setLeft] = useState(0);
+
+  const parentRectangle = useRef(null);
+  const childRectangle = useRef(null);
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+  }, []);
+
+  function handleKeyDown(event) {
+    if (!parentRectangle || !childRectangle) {
+      return;
+    }
+
+    const parentCoordinates = parentRectangle.current.getBoundingClientRect();
+    const childCoordinates = childRectangle.current.getBoundingClientRect();
+
+    switch (event.keyCode) {
+      case keys.left:
+        setLeft((left) => Math.max(left - STEP, 0));
+        return;
+      case keys.up:
+        setTop((top) => Math.max(top - STEP, 0));
+        return;
+      case keys.right:
+        setLeft((left) =>
+          Math.min(
+            left + STEP,
+            parentCoordinates.width - childCoordinates.width
+          )
+        );
+        return;
+      case keys.down:
+        setTop((top) =>
+          Math.min(
+            top + STEP,
+            parentCoordinates.height - childCoordinates.height
+          )
+        );
+        return;
+      default:
+        return;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-rectangle" ref={parentRectangle}>
+      <div
+        className="secondary-rectangle"
+        style={{ top, left }}
+        ref={childRectangle}
+      />
     </div>
   );
 }
