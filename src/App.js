445b1a8caccfb9dcb90e2962be782635dd63c20a
Sergey Khavrolev <sergey.khavrolev@coolbet.com>
2022-12-03 22:17:16 +0300

    delete usecallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 const STEP = 25;
@@ -17,47 +17,47 @@ function App() {
   const parentRectangle = useRef(null);
   const childRectangle = useRef(null);
 
-  const handleKeyDown = useCallback((event) => {
-    if (!parentRectangle.current || !childRectangle.current) {
-      return;
-    }
-
-    const parentCoordinates = parentRectangle.current.getBoundingClientRect();
-    const childCoordinates = childRectangle.current.getBoundingClientRect();
-
-    switch (event.keyCode) {
-      case keys.left:
-        setLeft((left) => Math.max(left - STEP, 0));
-        return;
-      case keys.up:
-        setTop((top) => Math.max(top - STEP, 0));
-        return;
-      case keys.right:
-        setLeft((left) =>
-          Math.min(
-            left + STEP,
-            parentCoordinates.width - childCoordinates.width
-          )
-        );
-        return;
-      case keys.down:
-        setTop((top) =>
-          Math.min(
-            top + STEP,
-            parentCoordinates.height - childCoordinates.height
-          )
-        );
-        return;
-      default:
-        return;
-    }
-  }, []);
-
   useEffect(() => {
+    function handleKeyDown(event) {
+      if (!parentRectangle.current || !childRectangle.current) {
+        return;
+      }
+
+      const parentCoordinates = parentRectangle.current.getBoundingClientRect();
+      const childCoordinates = childRectangle.current.getBoundingClientRect();
+
+      switch (event.keyCode) {
+        case keys.left:
+          setLeft((left) => Math.max(left - STEP, 0));
+          return;
+        case keys.up:
+          setTop((top) => Math.max(top - STEP, 0));
+          return;
+        case keys.right:
+          setLeft((left) =>
+            Math.min(
+              left + STEP,
+              parentCoordinates.width - childCoordinates.width
+            )
+          );
+          return;
+        case keys.down:
+          setTop((top) =>
+            Math.min(
+              top + STEP,
+              parentCoordinates.height - childCoordinates.height
+            )
+          );
+          return;
+        default:
+          return;
+      }
+    }
+
     window.addEventListener("keydown", handleKeyDown);
 
     return () => window.removeEventListener("keydown", handleKeyDown);
-  }, [handleKeyDown]);
+  }, []);
 
   return (
     <div className="main-rectangle" ref={parentRectangle}>
